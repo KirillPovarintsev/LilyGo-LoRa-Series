046af5fee3ba633d9184e2e8ac33d56e9076ab91
Kirill Povarintsev <kirill@nvplay.com>
2025-04-09 17:11:26 +1200

    MSVC test app

---
+++ msvc/ConsoleApplication1.cpp
@@ -0,0 +1,60 @@
+// ConsoleApplication1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <conio.h>
+
+#include "Menu.h"
+
+#define KEY_UP 72
+#define KEY_DOWN 80
+#define KEY_LEFT 75
+#define KEY_RIGHT 77
+#define KEY_ENTER 13
+#define KEY_ESC 27
+
+int main()
+{
+    Menu _menu;
+
+    auto& action1 = _menu.addItem("Action 1", []() {});
+    auto& action2 = _menu.addItem("Action 2", []() {});
+    auto& submenu1 = _menu.addMenu("Sub-menu 1");
+    auto& submenu1action1 = submenu1.addItem("Action 1-1", []() {});
+    auto& submenu1action2 = submenu1.addItem("Action 1-2", []() {});
+    auto& submenu1submenu1 = submenu1.addMenu("Sub-menu 1-1");
+    auto& submenu1submenu1action1 = submenu1submenu1.addItem("Action 1-1-1", []() {});
+    auto& submenu1submenu1action2 = submenu1submenu1.addItem("Action 1-2-1", []() {});
+
+    int c;
+
+    do
+    {
+        c = _getch();
+
+        switch (c)
+        {
+        case KEY_UP:
+            _menu.up();
+            break;
+        case KEY_DOWN:
+            _menu.down();
+            break;
+        case KEY_ENTER:
+            _menu.select();
+            break;
+        }
+
+    } while (c != KEY_ESC);
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
